065db29208923c7b24ec9e7a2e8df8f66720a28b
aaronfeingold <ajfeingold88@gmail.com>
2024-08-02 16:55:07 -0400

    idk why the buttons now fail...

--- src/shared/components/NavBar.tsx
+++ src/shared/components/NavBar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Disclosure, DisclosureButton } from "@headlessui/react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { HomeModernIcon } from "@heroicons/react/24/solid";
 
@@ -43,7 +47,7 @@ const NavBar: React.FC = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <Disclosure.Button className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -54,7 +58,7 @@ const NavBar: React.FC = () => {
                 aria-hidden="true"
                 className="hidden h-6 w-6 group-data-[open]:block"
               />
-            </DisclosureButton>
+            </Disclosure.Button>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
