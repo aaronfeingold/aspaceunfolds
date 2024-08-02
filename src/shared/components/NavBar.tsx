50df6b754d7a57845a1f59a1cbda88b82b810093
aaronfeingold <ajfeingold88@gmail.com>
2024-08-02 17:44:56 -0400

    z-20 on the navbar with fixed full width for sticky effect

--- src/shared/components/NavBar.tsx
+++ src/shared/components/NavBar.tsx
@@ -39,7 +39,7 @@ const NavBar: React.FC = () => {
   return (
     <Disclosure
       as="nav"
-      className={`fixed top-0 w-full transition-colors duration-300 ${
+      className={`fixed z-20 w-full transition-colors duration-300 ${
         isScrolled ? "bg-gray-700" : "bg-gray-800"
       }`}
     >
@@ -47,7 +47,7 @@ const NavBar: React.FC = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <Disclosure.Button className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -58,7 +58,7 @@ const NavBar: React.FC = () => {
                 aria-hidden="true"
                 className="hidden h-6 w-6 group-data-[open]:block"
               />
-            </Disclosure.Button>
+            </DisclosureButton>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
@@ -90,10 +90,10 @@ const NavBar: React.FC = () => {
         </div>
       </div>
 
-      <Disclosure.Panel className="sm:hidden">
+      <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pb-3 pt-2">
           {navigation.map((item: NavigationItem) => (
-            <Disclosure.Button
+            <DisclosureButton
               key={item.name}
               as="a"
               href={item.href}
@@ -106,10 +106,10 @@ const NavBar: React.FC = () => {
               )}
             >
               {item.name}
-            </Disclosure.Button>
+            </DisclosureButton>
           ))}
         </div>
-      </Disclosure.Panel>
+      </DisclosurePanel>
     </Disclosure>
   );
 };
